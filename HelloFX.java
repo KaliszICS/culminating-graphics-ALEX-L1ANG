76bc65d7ce58bcb7240fc8dcf275dd6a8775b862
ALEX-L1ANG <343293684@tdsb.ca>
2026-06-03 15:40:15 +0000

    update1

--- HelloFX.java
+++ HelloFX.java
@@ -5,20 +5,39 @@ Date Created: June 1, 2026
 Date Last Modified: June 1, 2026
 */
 
+// Design imports
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+// Code imports
+import java.util.Scanner;
+
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        // Graphic Design
+        Label l = new Label("test");
         Scene scene = new Scene(new StackPane(l), 640, 480);
+
+        // Game Code
+
+        // Hints
+        System.out.println("Across");
+        System.out.println("Down");
+        
+        // Game Board
+        char[][] crossword = {
+            {=},
+            {},
+            {},
+            {},
+            {}
+        };
+
         stage.setScene(scene);
         stage.show();
     }
