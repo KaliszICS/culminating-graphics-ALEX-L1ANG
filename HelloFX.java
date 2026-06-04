84dc5d651a1bad141247d84980068e41a5ff59fa
ALEX-L1ANG <343293684@tdsb.ca>
2026-06-04 15:31:58 +0000

    update2

--- HelloFX.java
+++ HelloFX.java
@@ -24,19 +24,39 @@ public class HelloFX extends Application {
         Scene scene = new Scene(new StackPane(l), 640, 480);
 
         // Game Code
+        
+        // Game Board
+        char[][] gameBoard = {
+            {'G', 'U', 'A', 'F', 'F'},
+            {'U', 'N', 'T', 'I', 'L'},
+            {'T', 'I', 'T', 'L', 'E'},
+            {'S', 'T', 'E', 'E', 'D'},
+            {'Y', 'A', 'R', 'D', '#'}
+        };
+
+        // Player Board
+        char[][] playerBoard = {
+            {' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' '}
+        };
 
         // Hints
         System.out.println("Across");
-        System.out.println("Down");
+        System.out.println("American tennis player Coco, winner of the 2025 French Open");
+        System.out.println("No later than");
+        System.out.println("Prizefighter's quest");
+        System.out.println("Pegasus, for one");
+        System.out.println("Lawn party locale");
         
-        // Game Board
-        char[][] crossword = {
-            {=},
-            {},
-            {},
-            {},
-            {}
-        };
+        System.out.println("Down");
+        System.out.println("Full of moxie");
+        System.out.println(" \"Sweet love\" singer Baker");
+        System.out.println("This is _____ nonsense!");
+        System.out.println("Submitted a tax return");
+        System.out.println("Skipped town in a hurry");
 
         stage.setScene(scene);
         stage.show();
