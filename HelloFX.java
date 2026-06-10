c48ac75de56709e8ca5835c262d5668bfbc2b9d2
ALEX-L1ANG <343293684@tdsb.ca>
2026-06-10 06:06:56 +0000

    menu test 1

--- HelloFX.java
+++ HelloFX.java
@@ -5,19 +5,29 @@ Date Created: June 1, 2026
 Date Last Modified: June 1, 2026
 */
 
-// Design imports
+// Graphic Design Imports
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-// Code imports
-import java.util.Scanner;
-
 public class HelloFX extends Application {
+
+    // Game Board Test
+    int gameSize = 5;
+    char[][] gameBoard = {
+        {'A', 'B', 'C', 'D', 'E'},
+        {'A', 'B', 'C', 'D', 'E'},
+        {'A', 'B', 'C', 'D', 'E'},
+        {'A', 'B', 'C', 'D', 'E'},
+        {'A', 'B', 'C', 'D', 'E'}
+    };
+    TextField[][] playerInputs = new TextField[5][5];
 
     @Override
     public void start(Stage stage) {
@@ -55,6 +65,44 @@ public class HelloFX extends Application {
         stage.setScene(scene);
         stage.setTitle("Crossword Game - Main Menu");
         stage.show();
+    }
+
+    // Method to switch from menu to game screen (refer to line 41)
+    public void showGameBoardScene(Stage stage) {
+        VBox gameLayout = new VBox(20);
+        gameLayout.setAlignment(Pos.CENTER);
+        gameLayout.setStyle("-fx-background-color: #ECF0F1;");
+
+        // Designing Game Title
+        Label gameTitle = new Label("Solve the Puzzle!");
+        gameTitle.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+
+        // Grid Layout
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER); // Centering grid position
+        grid.setHgap(5); // Horizontal width of gaps between columns
+        grid.setVgap(5); // Vertical width of gaps between rows
+
+        for (int i = 0; i < gameSize; i++) {
+            for (int j = 0; j < gameSize; j++) {
+                TextField box = new TextField();
+                box.setPrefWidth(45);
+                box.setPrefHeight(45);
+                box.setAlignment(Pos.CENTER);
+                box.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+
+                playerInputs[i][j] = box;
+                grid.add(box, j, i);
+            }
+        }
+
+        Button backButton = new Button("Back to Menu"); // Back to menu button 
+        backButton.setOnAction(e -> start(stage)); // Functionality of back to menu button
+
+        gameLayout.getChildren().addAll(gameTitle, grid, backButton);
+
+        Scene gameScene = new Scene(gameLayout, 640, 480);
+        stage.setScene(gameScene);
     }
 
     public static void main(String[] args) {
